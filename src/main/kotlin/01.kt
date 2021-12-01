f8c13e894e5234dde090d130dd9892b158a95ff8
Ben Gazzard <ben@dartoxia.com>
2021-12-01 05:08:47 +0000

    day 1

--- src/main/kotlin/01.kt
+++ src/main/kotlin/01.kt
@@ -1,3 +1,23 @@
 fun main() {
+    val input = getPuzzleInput(1, 1)
+    val ints = input.split("\n").map{it.toInt()}
+    var i = 1
+    var sinking = 0
+    while (i < ints.size) {
+        if (ints[i] > ints[i - 1]) {
+            sinking++
+        }
+        i++
+    }
+    println("Sinks $sinking times")
 
+    i = 3
+    sinking = 0
+    while (i < ints.size) {
+        if ((ints[i] + ints[i - 1] + ints[i - 2]) > (ints[i - 3] + ints[i - 1] + ints[i - 2])) {
+            sinking++
+        }
+        i++
+    }
+    println("Sinks $sinking times")
 }
